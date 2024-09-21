1d818d71ec811b0c03cef99aec549cca5dc150c7
GLeoni29 <277354@studenti.unimore.it>
2024-09-21 13:57:54 +0200

    Update client_sensor.py

--- Arduino/client_sensor.py
+++ Arduino/client_sensor.py
@@ -9,13 +9,30 @@ arduino = serial.Serial("COM5", 9600)
 
 
 ''' ------ URL BASE ------ '''
-#base_url = 'http://127.0.0.1:80'
+#ybase_url = 'http://127.0.0.1:80'
 base_url = 'https://coldar-436310.ew.r.appspot.com'
 
 
 ''' ------ MAIN ------ '''
 while True:
     if arduino.in_waiting > 0:
+        response = requests.post(f'{base_url}/invia_messaggio')
+        if response.status_code == 200:
+            message = response.text.strip()
+            print('messaggio: ', message)
+            if message == "LEDandBUZZER_ON":
+                arduino.write(b"O")
+            elif message == "LEDandBUZZER_OFF":
+                arduino.write(b"F")
+            elif message == "LED_ON":
+                arduino.write(b"L")
+            elif message == "LED_OFF":
+                arduino.write(b"M")
+            elif message == "BUZZER_ON":
+                arduino.write(b"B")
+            elif message == "BUZZER_OFF":
+                arduino.write(b"C")
+
         data = arduino.readline().decode('utf-8').strip()
         if len(data) == 1:
             continue
@@ -32,7 +49,7 @@ while True:
             requests.post(f'{base_url}/dati', rilevazione)
 
 
-        response = requests.post(f'{base_url}/invia_messaggio')
+        '''response = requests.post(f'{base_url}/invia_messaggio')
         if response.status_code == 200:
             message = response.text.strip()
             print('messaggio: ',message)
@@ -47,4 +64,4 @@ while True:
             elif message == "BUZZER_ON":
                 arduino.write(b"B")
             elif message == "BUZZER_OFF":
-                arduino.write(b"C")
+                arduino.write(b"C")'''
