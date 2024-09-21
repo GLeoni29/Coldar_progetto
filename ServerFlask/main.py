5e27bbfcf54fbd976a1c3e321c4ec42ec9fb1f99
GLeoni29 <277354@studenti.unimore.it>
2024-09-21 12:13:41 +0200

    Update main.py

--- ServerFlask/main.py
+++ ServerFlask/main.py
@@ -4,23 +4,25 @@ from google.cloud import firestore
 import time
 from datetime import datetime
 
+
 ''' ------ AVVIO ISTANZA FLASK ------ '''
 app = Flask(__name__)
 
+
 ''' ------ APERTURA CONNESSIONE FIRESTORE ------ '''
-db = 'test1'
-####################################################################db = 'Coldar'
+##############àdb = 'test1'
+db = 'coldar'
 coll = 'Arduino1'
 db = firestore.Client.from_service_account_json('credentials.json', database=db)
+
 
 ''' ------ VARIABILI GLOBALI ------ '''
 sportello_aperto_da = None  # Variabile per tenere traccia del tempo di apertura dello sportello
 stato_allarme = False  # variabile per segnalare che il sistema è in stato di allarme
 lista_messaggi = []  # lista per inviare comandi ad arduino
 
+
 ''' ------ RECUPERO DATI DA ARDUINO ------ '''
-
-
 def controlla_condizioni(temperatura, sportello_aperto):
     global lista_messaggi
     global sportello_aperto_da
@@ -61,7 +63,6 @@ def controlla_condizioni(temperatura, sportello_aperto):
     elif (not allarme_temperatura and not allarme_sportello) and not stato_allarme:
         pass  # mantieni lo stato di non allarme
 
-
 @app.route('/dati', methods=['POST'])
 def ricevi_dati():
     # Ricevi i dati dal client_sensor
@@ -80,58 +81,56 @@ def ricevi_dati():
     return "Dati salvati", 200
 
 
-# ------ HOME ------
+''' ------ HOME ------ '''
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-# ------ AREA MONITOR ------
+''' ------ AREA MONITOR ------ '''
 @app.route('/area_monitor')
 def area_monitor():
-    global sportello_aperto_da
+   global sportello_aperto_da
 
-    # Ottieni l'ultima rilevazione inserita nel database
-    collection_ref = db.collection(coll)
-    docs = collection_ref.order_by('dataora', direction=firestore.Query.DESCENDING).limit(1).stream()
+   # Ottieni l'ultima rilevazione inserita nel database
+   collection_ref = db.collection(coll)
+   docs = collection_ref.order_by('dataora', direction=firestore.Query.DESCENDING).limit(1).stream()
 
-    row = None
-    for doc in docs:
-        row = doc.to_dict()
-        break  # Prendi solo il primo documento
+   row = None
+   for doc in docs:
+       row = doc.to_dict()
+       break  # Prendi solo il primo documento
 
-    if row is None:
-        print("Nessun dato disponibile.")
-        return render_template('area_monitor.html', stato="ERRORE - Nessun dato", temperatura=None, sportello=None,
-                               orario=None)
+   if row is None:
+       print("Nessun dato disponibile.")
+       return render_template('area_monitor.html', stato="ERRORE - Nessun dato", temperatura=None, sportello=None,
+                              orario=None)
 
-    # Imposta messaggio di stato (area monitor)
-    stato = "ok con sportello chiuso"
-    if float(row['temperatura']) < 10 or float(row['temperatura']) > 25:
-        # if float(row['temperatura']) < 2 or float(row['temperatura']) > 8: # Range GDO
-        stato = "allarme temperatura"
-    elif int(row['sportello']) == 1:
-        if sportello_aperto_da is None:
-            sportello_aperto_da = time.time()
-        tempo_apertura = time.time() - sportello_aperto_da
-        if tempo_apertura > 30:
-            stato = "sportello aperto da più di 30 sec"
-        else:
-            stato = f"ok con sportello aperto da {int(tempo_apertura)} secondi"
-    else:
-        sportello_aperto_da = None
+   # Imposta messaggio di stato (area monitor)
+   stato = "ok con sportello chiuso"
+   if float(row['temperatura']) < 10 or float(row['temperatura']) > 25:
+       # if float(row['temperatura']) < 2 or float(row['temperatura']) > 8: # Range GDO
+       stato = "allarme temperatura"
+   elif int(row['sportello']) == 1:
+       if sportello_aperto_da is None:
+           sportello_aperto_da = time.time()
+       tempo_apertura = time.time() - sportello_aperto_da
+       if tempo_apertura > 30:
+           stato = "sportello aperto da più di 30 sec"
+       else:
+           stato = f"ok con sportello aperto da {int(tempo_apertura)} secondi"
+   else:
+       sportello_aperto_da = None
 
-    # Ottieni orario rilevazione
-    dataora = datetime.strptime(row['dataora'], '%Y-%m-%d %H:%M:%S.%f')
-    orario = dataora.strftime('%H:%M:%S')
+   # Ottieni orario rilevazione
+   dataora = datetime.strptime(row['dataora'], '%Y-%m-%d %H:%M:%S.%f')
+   orario = dataora.strftime('%H:%M:%S')
 
-    return render_template('area_monitor.html', stato=stato, temperatura=row['temperatura'], sportello=row['sportello'],
-                           orario=orario)
+   return render_template('area_monitor.html', stato=stato, temperatura=row['temperatura'], sportello=row['sportello'],
+                          orario=orario)
 
 
 ''' ------ AREA ANALISI ------ '''
-
-
 @app.route('/area_analisi')
 def area_analisi():
     # Prepara strutture dati per creare grafici
@@ -155,7 +154,6 @@ def area_analisi():
     # print("dati sportello: ", dati_sportello)
 
     return render_template('area_analisi.html', dati_temperatura=dati_temperatura, dati_sportello=dati_sportello)
-
 
 @app.route('/filtra_dati', methods=['POST'])
 def filtra_dati():
@@ -205,12 +203,9 @@ def filtra_dati():
 
 
 ''' ----- AREA TEST ----- '''
-
-
 @app.route('/area_test')
 def area_test():
     return render_template('area_test.html')
-
 
 @app.route('/test_led_on')
 def test_led_on():
@@ -218,13 +213,11 @@ def test_led_on():
     lista_messaggi.append("LED_ON")
     return render_template('area_test.html')
 
-
 @app.route('/test_led_off')
 def test_led_off():
     global lista_messaggi
     lista_messaggi.append("LED_OFF")
     return render_template('area_test.html')
-
 
 @app.route('/test_buzzer_on')
 def test_buzzer_on():
@@ -232,13 +225,11 @@ def test_buzzer_on():
     lista_messaggi.append("BUZZER_ON")
     return render_template('area_test.html')
 
-
 @app.route('/test_buzzer_off')
 def test_buzzer_off():
     global lista_messaggi
     lista_messaggi.append("BUZZER_OFF")
     return render_template('area_test.html')
-
 
 @app.route('/test_led_e_buzzer_on')
 def test_led_e_buzzer_on():
@@ -246,13 +237,11 @@ def test_led_e_buzzer_on():
     lista_messaggi.append("LEDandBUZZER_ON")
     return render_template('area_test.html')
 
-
 @app.route('/test_led_e_buzzer_off')
 def test_led_e_buzzer_off():
     global lista_messaggi
     lista_messaggi.append("LEDandBUZZER_OFF")
     return render_template('area_test.html')
-
 
 def check_messaggio(lista_messaggi):
     if len(lista_messaggi) == 1:
@@ -262,11 +251,13 @@ def check_messaggio(lista_messaggi):
         messaggio = ""
     return messaggio
 
-
 @app.route('/invia_messaggio', methods=['POST'])
 def invia_messaggio():
     messaggio = check_messaggio(lista_messaggi)
     return messaggio
+
+
+
 
 
 if __name__ == '__main__':
