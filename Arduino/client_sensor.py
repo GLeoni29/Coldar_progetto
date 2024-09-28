fdd33d944743bd70eac81e525eeccaa5f9e6d0ac
GLeoni29 <277354@studenti.unimore.it>
2024-09-28 08:36:46 +0200

    Update client_sensor.py

--- Arduino/client_sensor.py
+++ Arduino/client_sensor.py
@@ -9,16 +9,16 @@ arduino = serial.Serial("COM5", 9600)
 
 
 ''' ------ URL BASE ------ '''
-#ybase_url = 'http://127.0.0.1:80'
-base_url = 'https://coldar-436310.ew.r.appspot.com'
+base_url = 'http://127.0.0.1:80'
+#base_url = 'https://coldar-436310.ew.r.appspot.com'
 
 i = 0
 ''' ------ MAIN ------ '''
 while True:
     if arduino.in_waiting > 0:
-        #print(f"arduino disponibile: {i}")
+        '''#print(f"arduino disponibile: {i}")
         i +=1
-        response = requests.post(f'{base_url}/invia_messaggio', timeout=100)
+        response = requests.post(f'{base_url}/invia_messaggio')#, timeout=100)
         if response.status_code == 200:
             message = response.text.strip()
             print('messaggio',i,': ', message)
@@ -33,7 +33,7 @@ while True:
             elif message == "BUZZER_ON":
                 arduino.write(b"B")
             elif message == "BUZZER_OFF":
-                arduino.write(b"C")
+                arduino.write(b"C")'''
 
         data = arduino.readline().decode('utf-8').strip()
         if len(data) == 1:
@@ -47,9 +47,27 @@ while True:
                 'temperatura': float(temperatura),
                 'sportello': int(sportello)
             }
-            #print("rilevazione: ", rilevazione)
-            requests.post(f'{base_url}/dati', rilevazione)
+            print("rilevazione: ", rilevazione)
+            response = requests.post(f'{base_url}/dati', rilevazione)
 
+        #print(f"arduino disponibile: {i}")
+            i +=1
+            #response = requests.post(f'{base_url}/dati') #requests.post(f'{base_url}/invia_messaggio')#, timeout=100)
+            if response.status_code == 200:
+                message = response.text.strip()
+                print('messaggio',i,': ', message)
+                if message == "LEDandBUZZER_ON":
+                    arduino.write(b"O")
+                elif message == "LEDandBUZZER_OFF":
+                    arduino.write(b"F")
+                elif message == "LED_ON":
+                    arduino.write(b"L")
+                elif message == "LED_OFF":
+                    arduino.write(b"M")
+                elif message == "BUZZER_ON":
+                    arduino.write(b"B")
+                elif message == "BUZZER_OFF":
+                    arduino.write(b"C")
 
         '''response = requests.post(f'{base_url}/invia_messaggio')
         if response.status_code == 200:
